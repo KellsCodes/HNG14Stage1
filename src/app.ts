5f6254659bcbe72a87d362c5ea5b79f05e5d44e7
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-04-15 17:16:02 +0100

    fix: add 404 route

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,10 @@ app.use(responseTime());
 
 // Apply routes
 app.use("/", mainRouter);
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
